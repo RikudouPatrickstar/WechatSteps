5c37f41418348910f0762f43af25954c609ff703
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2022-11-06 16:58:40 +0800

    First commit

---
+++ run.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set WORK_DIR=%~dp0
+set LOG_FILE=%WORK_DIR%/log
+
+cd %WORK_DIR% || exit
+if not exist %LOG_FILE% (
+    python -m pip install -r requirements.txt
+)
+python wechat_steps.py >> %LOG_FILE%
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+WORK_DIR=$(cd "$(dirname "$0")" || exit; pwd)
+LOG_FILE=${WORK_DIR}/log
+
+# 进入工作目录（项目目录）
+cd "${WORK_DIR}" || exit
+
+# 判断环境并执行脚本
+if command -v python3 > /dev/null 2>&1;then
+    [ ! -f "${LOG_FILE}" ] && python3 -m pip install -r requirements.txt
+    python3 wechat_steps.py >> "${LOG_FILE}" 2>&1
+    exit 0
+elif command -v python > /dev/null 2>&1 && [ "$(python --version | sed 's_.* __;s_\..*__')" -eq "3" ];then
+    [ ! -f "${LOG_FILE}" ] && python -m pip install -r requirements.txt
+    python wechat_steps.py >> "${LOG_FILE}" 2>&1
+    exit 0
+else
+    echo "环境错误: 未找到可用的 Python3 环境"
+    exit 1
+fi
