3b4be7634dce2a3bd2111567e44d153606cb84e0
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2022-11-07 12:50:55 +0800

    run.cmd 脚本新增环境判断

--- run.cmd
+++ run.cmd
@@ -4,6 +4,11 @@ set WORK_DIR=%~dp0
 set LOG_FILE=%WORK_DIR%/data/log
 
 cd %WORK_DIR% || exit
+python --version >nul 2>nul
+if %ERRORLEVEL% == 1 (
+    echo Python 环境未安装
+    exit /b 1
+)
 if not exist %LOG_FILE% (
     python -m pip install -r requirements.txt
 )
